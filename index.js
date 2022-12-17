86fdeab236d3656b26c3a17dec8de0fe260b663d
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-12-17 13:30:12 -0600

    ejercicio3

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 console.log('Hola desde Js')
 
-
-let titulo = document.getElementById('titulo')
-console.log(titulo)
-
-let subtitulo = document.getElementsByClassName('rojo')
-
-console.log(subtitulo)
-
-console.log(document.getElementsByTagName('h1'))
-
-console.log(document.querySelector('h2'))
-console.log(document.querySelectorAll('h2'))
-
-
-let etiquetaDiv = document.createElement('div')
-let texto = document.createTextNode('Elemento creado con Js')
-
-etiquetaDiv.className = 'color-div'; 
-
-document.getElementById('container').appendChild(etiquetaDiv)
-
-etiquetaDiv.appendChild(texto)
-
-// document.getElementById("btn").addEventListener("click", function( event ) {
-//   console.log('soy un Click')
-//   console.log(event)
-//   event.target.innerHTML = 'Clicked!'
-// });
-
 function clicked (event) {
   console.log('click desde la funcion')
   alert('Hola soy un alert')
   console.log(event)
 }
+
+// localStorage 
+localStorage.setItem('miGato', 'Juan')
+let cat = localStorage.getItem('miGato')
+console.log(cat)
+// localStorage.removeItem('miGato')
+// localStorage.clear()
+
+
+function guardar() {
+  let inputValue = document.getElementById('inputText').value
+  let nombres = JSON.parse(localStorage.getItem('nombres')) ? JSON.parse(localStorage.getItem('nombres')) : []
+  console.log(inputValue)
+  nombres.push(inputValue)
+  console.log(nombres)
+  console.log(JSON.stringify(nombres))
+  localStorage.setItem('nombres', JSON.stringify(nombres))
+  document.getElementById('inputText').value = ''
+  render()
+}
+
+function render() {
+  let lista = document.getElementById('lista')
+  let nombres = JSON.parse(localStorage.getItem('nombres')) ? JSON.parse(localStorage.getItem('nombres')) : [] 
+  console.log(nombres)
+  lista.innerHTML = ''
+  nombres.forEach(element => {
+    lista.innerHTML += `
+      <p> ${element} </p>
+    `
+  });
+}
+
+
+render()
+
+
+
